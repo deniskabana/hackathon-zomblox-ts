3a02ffd2c2d51d8c93e1e55b95f6b130d605ffd3
Denis Kabana <denis.kabana@gmail.com>
2025-10-06 22:35:23 +0200

    Level manager basics

--- src/managers/LevelManager.ts
+++ src/managers/LevelManager.ts
@@ -1,4 +1,5 @@
 import { WORLD_SIZE } from "../config/gameGrid";
+import { gameInstance } from "../main";
 import type { LevelState } from "../types/LevelState";
 
 export default class LevelManager {
@@ -11,5 +12,20 @@ export default class LevelManager {
       phase: 'night',
       daysCounter: 0,
     }
+  }
+
+  public endNight() {
+    if (this.levelState.phase !== 'night') return;
+    this.levelState.phase = 'day';
+    this.levelState.daysCounter += 1;
+    gameInstance.MANAGERS.GameManager.stateSetPlaying('day')
+    // TODO: UI and game changes
+  }
+
+  public endDay() {
+    if (this.levelState.phase !== 'day') return;
+    this.levelState.phase = 'night'
+    gameInstance.MANAGERS.GameManager.stateSetPlaying('night')
+    // TODO: UI and game changes
   }
 }
